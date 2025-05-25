ca78937ba1cbf6a4e93e87e852de4fcfd13e7d69
UPBeat Dev <upbeat@kbps.io>
2025-05-24 20:01:14 -0400

    Fix single byte[] logging

--- libraries/UPBeatLogger.groovy
+++ libraries/UPBeatLogger.groovy
@@ -6,7 +6,6 @@
  * Author: UPBeat Automation
  */
 import groovy.transform.Field
-import java.util.regex.Pattern
 
 library (
         name: "UPBeatLogger",
@@ -18,22 +17,59 @@ library (
 
 @Field static final Map LOG_LEVELS = [0:"Off", 1:"Error", 2:"Warn", 3:"Info", 4:"Debug", 5:"Trace"]
 @Field static final int LOG_DEFAULT_LEVEL = 0
-@Field static final Pattern HHU_PATTERN = Pattern.compile('%([-#+ 0]*)(\\d*)(\\.\\d*)?hhu')
 
-// Formats strings, handles %hhu for unsigned bytes, and byte arrays printed as hex
+String sprintf(String format, Object arg){
+    if (arg instanceof Byte) {
+        arg & 0xFF
+    } else if (arg instanceof byte[]) {
+        arg = "[${arg.collect { String.format("0x%02X", it & 0xFF) }.join(", ")}]"
+    } else {
+        arg
+    }
+    String.format(format, arg)
+}
+
 String sprintf(String format, Object... args) {
     def processedArgs = args.collect { arg ->
         if (arg instanceof Byte) {
-            arg & 0xFF // Convert single Byte to unsigned (0-255)
+            arg & 0xFF
         } else if (arg instanceof byte[]) {
-            // Convert byte[] to comma-separated 0x%02X values
-            ((byte[])arg).collect { String.format("0x%02X", it & 0xFF) }.join(", ")
+            arg = "[${arg.collect { String.format("0x%02X", it & 0xFF) }.join(", ")}]"
         } else {
             arg
         }
     }
-    def modifiedFormat = format.replaceAll(HHU_PATTERN, '%$1$2$3d')
-    String.format(modifiedFormat, processedArgs as Object[])
+    String.format(format, processedArgs as Object[])
+}
+
+void logError(String format, Object arg) {
+    if (logLevel.toInteger() >= 1) {
+        log.error(arg ? sprintf(format, arg) : format)
+    }
+}
+
+void logWarn(String format, Object arg) {
+    if (logLevel.toInteger() >= 2) {
+        log.warn(arg ? sprintf(format, arg) : format)
+    }
+}
+
+void logInfo(String format, Object arg) {
+    if (logLevel.toInteger() >= 3) {
+        log.info(arg ? sprintf(format, arg) : format)
+    }
+}
+
+void logDebug(String format, Object arg) {
+    if (logLevel.toInteger() >= 4) {
+        log.debug arg ? sprintf(format, arg) : format
+    }
+}
+
+void logTrace(String format, Object arg) {
+    if (logLevel.toInteger() >= 5) {
+        log.trace arg ? sprintf(format, arg) : format
+    }
 }
 
 void logError(String format, Object... args) {
